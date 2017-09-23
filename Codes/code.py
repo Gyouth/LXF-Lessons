5d7f04a15be0a7107d689611809333dc8033d571
Gyouth <365282758gyh@gmail.com>
2017-09-23 14:36:53 +0800

    add example code until https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000/001431658624177ea4f8fcb06bc4d0e8aab2fd7aa65dd95000

---
+++ Codes/code.py
@@ -0,0 +1,49 @@
+# #to run the .py file directly on Linux/Mac
+# #!/usr/bin/env python3
+# #type the command: chmod a+x .py, in the terminal
+# # print
+# print("hello, world")
+# #print a space at the comma between strings
+# print("The quick brown box", "jumps over", "the lazy dog")
+# print(300)
+# print(100 + 200)
+# print("100 + 200 = ", 100 + 200)
+# #input
+# name = input()
+# print("hello", name)
+# name = input("Please enter your name")
+# print("hello", name)
+# print("1024 * 768 = ", 1024*768)
+# print("I\'m \"Ok\"")
+# print("I\'m learing\nPython")
+# print("\\\n\\")
+# print('\\\t\\')
+# print(r'\\\t\\')
+# #input in multi-line
+# print('line1' \
+# 	', line2' \
+# 	', line3')
+# #input a multi-line string
+# print('''line1,
+# 	line2,
+# 	line3''')
+
+# PI = 3.14159265359
+# print(10 / 3)
+# print(9 / 3)
+# print(10 // 3)
+# print(10 % 3)
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
